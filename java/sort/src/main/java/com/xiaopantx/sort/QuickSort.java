68b9dd7f6a35293f859c58ed3f27ffdd73246c11
xiaopantx <m15132582590@163.com>
2022-09-30 14:50:15 +0800

    quicksort

---
+++ java/sort/src/main/java/com/xiaopantx/sort/QuickSort.java
@@ -0,0 +1,20 @@
+package com.xiaopantx.sort;
+
+/**
+ *
+ * 快速排序算法
+ *  1. 单路快排
+ *      快速排序的缺点
+ *  2. 双路快排
+ *  3. 三路快排
+ *
+ * pivot position
+ * @author xiaopantx
+ * @version 1.0
+ */
+public class QuickSort {
+
+    private QuickSort() {}
+
+
+}
