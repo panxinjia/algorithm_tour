61296b26ecf06d243eadaab81281acc0aa7217e0
xiaopantx <m15132582590@163.com>
2022-10-05 17:30:45 +0800

    bs

--- java/search/src/main/java/com/xiaopantx/search/BinarySearch.java
+++ java/search/src/main/java/com/xiaopantx/search/BinarySearch.java
@@ -9,6 +9,22 @@ public class BinarySearch {
 
     // 二分查找, 非递归写法
     public static <E extends Comparable<E>> int search(E[] data, E key) {
+        if (data == null || data.length == 0) {
+            return -1;
+        }
+        int l = 0, r = data.length - 1;
+        int mid = (l + (r - l)) / 2;
+
+        while (l <= r) {
+            if (data[mid].compareTo(key) > 0) {
+
+            }else if (data[mid].compareTo(key) < 0) {
+
+            }else {
+                return mid;
+            }
+        }
+
         return -1;
     }
 
