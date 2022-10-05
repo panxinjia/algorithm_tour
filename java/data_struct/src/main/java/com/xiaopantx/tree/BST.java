e8977353d5bd5d647122968a7db12a8679283b58
xiaopantx <m15132582590@163.com>
2022-10-05 21:15:01 +0800

    bst

--- java/data_struct/src/main/java/com/xiaopantx/tree/BST.java
+++ java/data_struct/src/main/java/com/xiaopantx/tree/BST.java
@@ -1,31 +1,81 @@
 package com.xiaopantx.tree;
 
 /**
- * 二分搜索树 -> 最基本的树形结构
+ *  二叉树:
  *
  *  树结构在计算机科学领域应用广泛, 需要非常数量的掌握
  *
+ *  满二叉树:   除叶子节点外, 每个节点都有两个孩子节点
+ *  完全二叉树:
+ *
+ *
+ *  二分搜索树:
+ *      1. 二叉树
+ *      2. 二分搜索数每个节点的值, 大于左子树所有节点, 小于右子树的所有节点
+ *
+ * TODO 二分搜索树非递归写法
  * @author xiaopantx
+ *
  */
-public class BST<E> {
+public class BST<E extends Comparable<E>> {
+
+    private Node root;
+    private int size;
+
+    public BST() {
+        root = null;
+        size = 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // 添加
+    public void add(E e) {
+
+    }
+
+    // 修改
+
+    // 删除
 
 
-    public BST() {}
+    // 前序遍历
+    public void preOrder() {}
+
+    // 中序遍历
+
+    // 后序遍历
+
+
+    // 字符串打印
+    @Override
+    public String toString() {
+        // TODO
+        StringBuilder res = new StringBuilder();
+        return res.toString();
+    }
 
 
 
     private class Node {
         public Node left;
-        public E data;
+
+        public E e;
         public Node right;
 
-        public Node(E data) {
-            this(null, data, null);
+        public Node(E e) {
+            this(null, e, null);
         }
 
-        public Node(Node left, E data, Node right) {
+        public Node(Node left, E e, Node right) {
             this.left = left;
-            this.data = data;
+            this.e = e;
             this.right = right;
         }
     }
